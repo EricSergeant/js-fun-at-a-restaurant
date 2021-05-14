6f8a9e5cc78f451edcfd8c0ab2f90d6683375428
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-05-14 09:11:36 -0500

    Add completed meal.js and unskipped tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,55 @@
-function nameMenuItem() {
-
+function nameMenuItem(food) {
+  return `Delicious ${food}`;
 }
 
+function createMenuItem(ingredient, cost, time) {
+     var menuItem = {
+       name: undefined,
+       price: undefined,
+       type: undefined
+  }
+      menuItem.name = ingredient;
+      menuItem.price = cost;
+      menuItem.type = time;
+
+      return menuItem;
+}
+
+function addIngredients(moreIngred, target) {
+  var ingredients = [];
+  if (target.includes(moreIngred)) {
+    return ingredients;
+  } else {
+  return target.push(moreIngred);
+  }
+}
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`;
+}
+
+function decreasePrice(preSalePrice) {
+  return preSalePrice * 0.9;
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  var recipe = {
+    title: undefined,
+    ingredients: undefined,
+    type: undefined
+  }
+  recipe.title = title;
+  recipe.ingredients = ingredients;
+  recipe.type = menuItemType;
+
+  return recipe;
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
