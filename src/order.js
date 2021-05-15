2701c3406348f79a9a38923a30041bf60f089a53
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-05-15 11:56:26 -0500

    Complete order.js testing

--- src/order.js
+++ src/order.js
@@ -1,7 +1,43 @@
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+    deliveryOrders.push(order);
+  }
+  return deliveryOrders;
+}
+
+function refundOrder(orderNum, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].orderNumber === orderNum) {
+      deliveryOrders.splice([i], 1);
+    }
+  }
+  return deliveryOrders;
+}
+
+function listItems(deliveryOrders) {
+  var items = [];
+  for (var i = 0; i <deliveryOrders.length; i++) {
+    items.push(deliveryOrders[i].item);
+  }
+  var results = items.join(", ");
+  return results;
+}
+
+function searchOrder(orders, items) {
+  var deliveryOrders = [];
+  for (var i = 0; i < orders.length; i++) {
+    deliveryOrders.push(orders[i].item);
+  }
+  if (deliveryOrders.includes(items)) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
