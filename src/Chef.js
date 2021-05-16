60491bc913d21581e7f018d95b5e616510935c4d
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-05-16 15:09:03 -0500

    Pass Chef.js tests

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,27 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
   }
+//questions: the constructor allows us to make new objects, but these aren't returned in the code.  Are they avilable outside of the class?  Are they if return keyword was added within the constructor?
+
+greetCustomer(name, bool) {
+  if (bool === true) {
+    return `Good morning, ${name}!`;
+} else {
+  return `Hello, ${name}!`;
+}
+}
+
+checkForFood(item) {
+  //note: it seems to be pulling info from restaurant.js per test file line 7
+  if(this.restaurant.menus[item.type].includes(item)) {
+    return `Yes, we're serving ${item.name} today!`;
+  } else {
+    return `Sorry, we aren't serving ${item.name} today.`;
+  }
+}
+
 }
 
 module.exports = Chef;
